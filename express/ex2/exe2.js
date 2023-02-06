9c2bceca6fa0e2a3e690521fbf2758045d403701
Herevar <123502098+Herevar@users.noreply.github.com>
2023-02-06 09:42:23 +0100

    zadania megak express

---
+++ express/ex2/exe2.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const {calcRouter} = require('./routes/calc')
+
+const app = express();
+
+
+app.use(express.json())  // musiby byc zeby 'wiedział i obsłużył' format jsonowy
+app.use('/calc', calcRouter);
+app.use(express.static('./public',{
+    index : 'index.html'
+}));
+
+
+app.listen(3000, 'localhost')
